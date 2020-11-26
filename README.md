0f1e453003bc57c53b52d964c5043dd328448cff
Bryan Chiu <1930893235@qq.com>
2020-11-26 20:04:35 +0800

    优化

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SSLIVE2020-Special
 Use the python flask
+
+使用python flask框架做后端
+
+贡献者名单:(contributor list)
+
+1. Qiu Yiran (![@Nameber](https://github.com/Nambers))
--- files/console/console.js
+++ files/console/console.js
@@ -1,36 +1,37 @@
 // console.js
 function logOut() {
-    //登出
+	//登出
 	//取消这3个cookie，通过把过期设为-1
-    $.cookie('user', "", {
-        expires: -1,
-        path: '/'
-    });
-    $.cookie('time', "", {
-        expires: -1,
-        path: '/'
-    });
-    $.cookie('token', "", {
-        expires: -1,
-        path: '/'
-    });
-    location.reload();
+	$.cookie('user', "", {
+		expires: -1,
+		path: '/'
+	});
+	$.cookie('time', "", {
+		expires: -1,
+		path: '/'
+	});
+	$.cookie('token', "", {
+		expires: -1,
+		path: '/'
+	});
+	location.reload();
 }
-function changelive(){
-    $.ajax({
-        url: "/livestart",
-        method: "POST",
-        success: function (data) {
-            inLive = (inLive==1?0:1);
-            if (data == "success") {
-                if (inLive == 1) {
-                    $("#LiveControlButton").text("停 止直 播");
-                    $("#LiveStatusReminder").css("visibility", "visible");
-                }else{
-                    $("#LiveControlButton").text("开 始 直 播");
-                    $("#LiveStatusReminder").css("visibility", "hidden");
-                }
-            }
-        }
-    });
+
+function changelive() {
+	$.ajax({
+		url: "/livestart",
+		method: "POST",
+		success: function(data) {
+			inLive = (inLive == 1 ? 0 : 1);
+			if (data == "success") {
+				if (inLive == 1) {
+					$("#LiveControlButton").text("停 止直 播");
+					$("#LiveStatusReminder").css("visibility", "visible");
+				} else {
+					$("#LiveControlButton").text("开 始 直 播");
+					$("#LiveStatusReminder").css("visibility", "hidden");
+				}
+			}
+		}
+	});
 }
