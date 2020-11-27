87de2211c879f5659dd939fdd06c77ed18f2ae8a
Bryan Chiu <1930893235@qq.com>
2020-11-27 08:46:33 +0800

    优化(增加缓存功能，代码结构优化)

--- Main.py
+++ Main.py
@@ -1,110 +1,119 @@
 # coding: utf-8
 from datetime import timedelta
-
+import config
+from flask_cache import Cache
 from flask import Flask, render_template, request, make_response
-import hashlib
 import random
 import time
+
 # 可参考: https://blog.csdn.net/qq_40832960/article/details/107132488
+cache = Cache()
 # Flask实例
-app = Flask(__name__, template_folder="www", static_folder='files', static_url_path="/files")
-# 直播状态
-inLive = 0
-# 账号密码字典
-password = {"suit": "lovesuit", "a": "b"}
-# 字符key
-random_chars = "fsajfsjhs"
-# 设置缓存时间
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(hours=12)
+app = Flask(__name__, template_folder=config.root, static_folder=config.static_root,
+            static_url_path=config.static_url_root)
+# 缓存初始化
+cache.init_app(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': config.cache_out_time})
+# 设置静态文件缓存时间
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(days=config.cache_time[0], hours=config.cache_time[1],
+                                                    minutes=config.cache_time[2])
 
 
 # 路径对应的执行函数，有路径就对应路径名，没路径就对应index
 # 如@app.route('/login') 对应def login()
 @app.route('/')
+@cache.cached()
 def index():
     # 首页判断
-    global inLive
-    if inLive == 0:
-        return render_template('introduction.html', inLive=inLive)
+    if config.inLive == 0:
+        return render_template('introduction.html', inLive=config.inLive)
     else:
+        # 跳转到直播
         return live()
 
 
 @app.route('/license')
-def linence():
+@cache.cached()
+def license_page():
     # 授权
     return render_template('license.html')
 
 
 @app.route('/robots.txt')
+@cache.cached()
 def robots():
     # 爬虫文件
     return render_template('robots.txt')
 
 
 @app.route('/about')
+@cache.cached()
 def about():
     # 关于我们页面
-    global inLive
-    return render_template('about.html', inLive=inLive)
+    return render_template('about.html', inLive=config.inLive)
 
 
 @app.route('/IE')
+@cache.cached()
 def IE():
     # IE提示页面
     return render_template('IE.html')
 
 
 @app.route('/live')
+@cache.cached()
 def live():
     # 直播页面
     return render_template('live.html')
 
 
 @app.route('/admin')
+@cache.cached()
 def console():
     # 管理路径
-    global inLive
     if not Check():
         # 进入登录页面
         return render_template('login.html')
     else:
-        return render_template('console.html', inLive=inLive)
+        return render_template('console.html', inLive=config.inLive)
+
 
 @app.route('/introduction')
-def introdiction():
-    global inLive
-    return render_template("introduction.html", inLive=inLive)
+@cache.cached()
+def introduction():
+    return render_template("introduction.html", inLive=config.inLive)
 
 
+# 开启直播路径，接受post
 @app.route('/livestart', methods=['POST'])
-def livestart():
-    global inLive
+@cache.cached()
+def liveStart():
     if not Check():
         # 防止在没token的情况开启直播
         return "Cookies 失效"
-    if inLive == 0:
-        inLive = 1
+    # 更改直播状态
+    if config.inLive == 0:
+        config.inLive = 1
     else:
-        inLive = 0
+        config.inLive = 0
     return "success"
 
 
 @app.route('/login', methods=['POST'])
+@cache.cached()
 def login():
     # 取前端传值
-    pw = request.form.get("PW","null", str)
+    pw = request.form.get("PW", "null", str)
     if pw == "null":
         return False
     # 循环字典
-    for b in password.keys():
+    for b in config.password.keys():
         # 取密码
-        c = password.get(b)
+        c = config.password.get(b)
         # 验证字典
-        if pw == md5(b + "#" + c):
-            res = make_response("success") # cookies 实例
-            timel = str(time.time()) # 取现在时间
-            max_s = 60 * 60 * 60 * 24 * 7  # 有效期7天
+        if pw == config.md5(b + "#" + c):
+            res = make_response("success")  # cookies 实例
+            timel = str(time.time())  # 取现在时间
+            max_s = 60 * 60 * 24 * config.cookies_time  # 设置有效期，秒为单位
             # 保存cookies
             res.set_cookie("user", b, max_age=max_s)
             res.set_cookie("token", Check(1, b, timel), max_age=max_s)
@@ -113,33 +122,27 @@ def login():
     return "你来了不该来的地方"
 
 
-def md5(a):
-    # md5 加密
-    return hashlib.md5(a.encode(encoding="UTF-8")).hexdigest()
-
-
 def Check(a=0, user="", timel=""):
     # 验证token
-    global password
-    global random_chars
     if a == 0:
         user = request.cookies.get('user')
         timel = request.cookies.get('time')
         token = request.cookies.get('token')
         # 用户名不在集
-        if user not in password.keys():
+        if user not in config.password.keys():
             return False
         if (float(time.time()) - float(timel)) < 0:
             # 建立时间比现在还未来
             return False
-        if (float(time.time()) - float(timel)) / 86400000 > 7:
+        if (float(time.time()) - float(timel)) / 86400000 > config.cookies_time:
             # 过期
             return False
+        # 随机数key
         key = token[0:3]
-        return key + md5(key + user + random_chars + password.get(user) + timel) == token
+        return config.encryption(key, user, timel) == token
     elif a == 1:
         rkey = str(random.randint(100, 999))
-        return rkey + md5(rkey + user + random_chars + password.get(user) + timel)
+        return config.encryption(rkey, user, timel)
     else:
         # a不正常
         return False
---
+++ config.py
@@ -0,0 +1,38 @@
+# 网站配置文件
+import hashlib
+
+# 直播初始状态
+inLive = 0
+# 账号密码字典集
+password = {"suit": "lovesuit", "a": "b"}
+# cookie加密用，字符key，更改会使之前用这个key加密的cookies失效
+random_chars = "fsajfsjhs"
+# 静态文件缓存时间，[day,hour,minutes]
+cache_time = [0, 12, 0]
+# 缓存过期时间，秒为单位
+cache_out_time = 300
+# root folder网址根目录文件夹名称
+root = "www"
+# static_folder静态文件(css,js,img...)文件夹名称
+static_root = "files"
+# static url root静态文件网页引用地址
+static_url_root = "/files"
+# cookies有效期，天为单位
+cookies_time = 7
+
+
+def md5(a):
+    # md5 加密
+    return hashlib.md5(a.encode(encoding="UTF-8")).hexdigest()
+
+
+# cookies中token加密算法
+# rkey - 随机数key
+# user - 用户名
+# rchar - 管理员自定义字符key
+# pw - 密码
+# timel - 时间戳
+def encryption(rkey, user, timel):
+    rchar = random_chars
+    pw = password.get(user)
+    return rkey + md5(rkey + user + rchar + pw + timel)
