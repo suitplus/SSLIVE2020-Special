0e55421f1539865577e5a200a28d225642c93d96
Bryan Chiu <1930893235@qq.com>
2020-11-28 23:47:15 +0800

    修复

--- files/src/js/live.js
+++ files/src/js/live.js
@@ -18,8 +18,66 @@ function load() {
         controls: true,
         preload: true, // 预加载
         poster: "/files/src/img/background/newintro.jpg", // 预览图
-        fullscreen: { options: { navigationUI: 'show' } }
-    });
+        fullscreen: { options: { navigationUI: 'show' } },
+        },
+        function onPlayerReady() {
+            // videojs.log('Your player is ready!');
+            //
+            // this.on("loadstart", function () {
+            //     console.log("开始请求数据 ");
+            // })
+            // this.on("progress", function () {
+            //     console.log("正在请求数据 ");
+            // })
+            // this.on("loadedmetadata", function () {
+            //     console.log("获取资源长度完成 ")
+            // })
+            // this.on("canplaythrough", function () {
+            //     console.log("视频源数据加载完成")
+            // })
+            // this.on("waiting", function () {
+            //     console.log("等待数据")
+            // });
+            this.on("play", function () {
+                //console.log("视频开始播放")
+                player.play();
+                $('#play').css('background-image', "url('/files/src/img/control/pause.svg')");
+                isplay = true;
+            });
+            // this.on("playing", function () {
+            //     //console.log("视频播放中")
+            // });
+            this.on("pause", function () {
+                // console.log("视频暂停播放")
+                player.pause();
+                $('#play').css('background-image', "url('/files/src/img/control/play.svg')");
+                isplay = false;
+            });
+            // this.on("ended", function () {
+            //     console.log("视频播放结束");
+            // });
+            this.on("error", function () {
+                console.log("加载错误")
+            });
+            // this.on("seeking", function () {
+            //     console.log("视频跳转中");
+            // })
+            // this.on("seeked", function () {
+            //     console.log("视频跳转结束");
+            // })
+            // this.on("ratechange", function () {
+            //     console.log("播放速率改变")
+            // });
+            // this.on("timeupdate", function () {
+            //     console.log("播放时长改变");
+            // })
+            // this.on("volumechange", function () {
+            //     console.log("音量改变");
+            // });
+            this.on("stalled", function () {
+                console.log("网速异常");
+            });
+        });
     //地址 http://suit.ssersay.cn/AppName/StreamName.m3u8?auth_key={鉴权串}
     player.src("https://suit.ssersay.cn/SUIT/stream.m3u8");//直播地址
 	//连接CCTV-1测试http://ivi.bupt.edu.cn/hls/cctv1.m3u8
